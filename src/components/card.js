da523fbe9191ddb2408890be13607f4da27d43a7
Mark Weigl <markwco@icloud.com>
2017-04-22 12:29:27 -0600

    setup basic project

---
+++ src/components/card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Card() {
+    const text  = 'Example card';
+    return (
+        <div className="card">
+            {text}
+        </div>
+    );
+};
